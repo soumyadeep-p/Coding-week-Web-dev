0421a9685a2f0642fd8e5c74d55232e400be459b
soumyadeep <p.soumyadeep@iitg.ac.in>
2022-06-04 23:53:18 +0530

    first comit

---
+++ home.js
@@ -0,0 +1,16 @@
+function searchfunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("searchbar");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("products");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByClassName("productname")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
